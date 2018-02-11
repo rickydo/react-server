991cda196aba0845084f4ec12f44d47bbd5ea36a
Ricky Do <ricky.dop@gmail.com>
2018-02-10 18:52:37 -0600

    working on register function and removing non relevant code in password component

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ build directory is the stuff the server is going to be using
 EJS is a templating system that allows dynamic view output
 
 Views is the default folder it expects
+
+Idiomatic JS files are files that you use in multiple different places such as
+on the client and on the server.
+This way you can use single files to work on both sides
+
+How to make this app Idiomatic?
---
+++ src/react/auth.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import PropTypes from 'prop-types';
+
+class Layout extends React.Component {
+  // expect to get a child
+  propTypes: {
+    children: PropTypes.string.isRequired
+  };
+
+  render(){
+    return(
+      <div className="overlay">
+        <div className="box">
+          <h2>{this.props.title}</h2>
+          {this.props.children}
+        </div>
+        <footer>By signing up, you agree to our
+          <a href="/terms">Terms of Service</a> and
+          <a href="/privacy">Privacy Policy</a>.
+        </footer>
+      </div>
+    )
+  }
+};
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      remember: true
+    }
+  }
+
+  render(){
+    return(
+      <form action="/u/login" method="post" acceptCharset="utf-8">
+      <Email />
+      <Pssword />
+      <label htmlFor="remember-me">
+        <input type="checkbox" name="remember_me" value="1"
+          id="remember-me" defaultChecked/>
+          Remember Me
+        </label>
+        <p>
+          <a href="register.html" className='inlink'>Register</a> |
+          <a href="#request-reset-password" className="inlink"> Reset Password</a>
+        </p>
+        <button type="submit">Login</button>
+      </form>
+    )
+  }
+};
+
+
+class Email extends React.Component {
+  render(){
+    return(
+      <input type="email" name="email" data-info="An active email account is needed to gain access."
+        placeholder="Your email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+        required title="You need to have a working email to gain access." required="required" id="email"/>
+    )
+  }
+};
+
+class Pssword extends React.Component {
+  // no constructor or state is needed on the server
+
+  render(){
+    var ch = [<input type="password" name="password1" required="required" placeholder="Password"
+              key="password" onChange={this.onChange} />];
+    if(this.props.twice){
+      ch[1] = [<input type="password" name="password2" required="required" placeholder="Repeat Password"
+                key="password2" onChange={this.onChange} />]
+    }
+    if(this.props.msg){
+      ch.push(<div className="error-message">{this.props.msg}</div>)
+    }
+
+    return(
+      <div>{ch}</div>
+    )
+  }
+
+};
+
+class Register extends React.Component {
+  render(){
+    return(
+      <form method="post" acceptCharset="utf-8" action="/u/register">
+        <Email />
+        <Pssword twice="1" msg={this.props.msg}/>
+        <span className="col-p100 info"></span>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+};
+
+
+ReactDOMServer.render(
+  (<Layout title="Login">
+    <Login />
+  </Layout>), document.getElementById('app')
+);
+
+
+module.exports = {
+  register: function(msg) {
+    return ReactDOMServer.renderToString(<Register msg={msg}/>);
+  }
+}
