c1daf6f13b373f84c35e07ee7bc85d7818ca0bcc
Ricky Do <ricky.dop@gmail.com>
2018-02-10 18:29:36 -0600

    have server up and running

--- src/server.js
+++ src/server.js
@@ -2,3 +2,21 @@ const express = require('express'),
       app = express(),
       mongojs = require('mongojs'),
       bodyParser = require('body-parser');
+
+// serve what's in the public folder
+app.use(express.static('public'));
+
+// when things are posted make sure we can access it
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.route('/register')
+  .get((req, result) => {
+    result.send("Get it!");
+  })
+  .post((req, result) => {
+    result.send("Post it!");
+  })
+
+app.listen(3001, () => {
+  console.log("Our server is running on 3001!");
+})
